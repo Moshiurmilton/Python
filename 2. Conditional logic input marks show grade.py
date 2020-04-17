b982a767b174d08d8d274d370b35caf6e7da6b73
Moshiur Rahman <44867753+Moshiurmilton@users.noreply.github.com>
2020-04-17 11:46:56 +0600

    Add files via upload

---
+++ 2. Conditional logic input marks show grade.py
@@ -0,0 +1,14 @@
+marks= input("Please enter your marks: ")
+marks = int(marks)
+if marks >= 80
+grade = "A+"
+elif marks>= 70
+grade = "A"
+elif marks >= 60
+grade = "A-"
+elif marks >= 50
+grade = "B"
+elif marks >= 40
+grade = "C"
+else grade = "F"
+print("Your grade is", grade)
